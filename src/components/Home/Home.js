990d84d2ac29b7c011cbb7fe090132af7eccd8b2
ionutprilipceanu <ionutprilipceanu@users.noreply.github.com>
2020-10-13 14:54:23 +0300

    up

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -39,7 +39,7 @@ class Home extends Component {
                 <p>
                   Ionuț Prilipceanu <br/>
                   (+40) 751 684 142 <br/>
-                  E-mail: prilipceanu.ionut@gmail.com
+                  prilipceanu.ionut@gmail.com
                 </p>
               </div>
             </div>
