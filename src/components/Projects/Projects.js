6cbc1370ac857d00c6333e13b23eb627df28c5ec
ionutprilipceanu <ionutprilipceanu@users.noreply.github.com>
2020-10-15 16:57:30 +0300

    up

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,9 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { Card, Button} from 'react-bootstrap'
 import "./Projects.css"
 import Fade from "react-reveal/Fade"
-import photographyPage from "..//Projects/images/photographyPage.jpg"
-import handcraftedLeader from "..//Projects/images/handcraftedLeader.jpg"
-import filterPage from "..//Projects/images/filterPage.jpg"
+import photographyPage from "..//..//..//static/images/photographyPage.jpg"
+import handcraftedLeader from "..//..//..//static/images/handcraftedLeader.jpg"
+import filterPage from "..//..//..//static/images/filterPage.jpg"
 
 
 
