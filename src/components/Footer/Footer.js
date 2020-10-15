fcd98d8052822658d2709bd6fec7c9997d507955
ionutprilipceanu <ionutprilipceanu@users.noreply.github.com>
2020-10-15 13:29:02 +0300

    up

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Footer.css'
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import Link from 'gatsby-link';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <ul className="componenteDesktop">
+        <p>&copy;Iași România 2020</p>
+      </ul>
+
+       <ul className="logoItemsDesktop">
+        <li><a href="https://www.facebook.com/mih.marian86" target="_blank" rel="noreferrer" className="logoItemDesktop"> <FontAwesomeIcon icon={faFacebook} /> </a></li>
+        <li><a href="https://www.facebook.com/mih.marian86" target="_blank" rel="noreferrer" className="logoItemDesktop"> <FontAwesomeIcon icon={faInstagram} /> </a></li>
+      </ul> 
+
+      <ul className="componentePhone">
+      </ul>
+
+       <ul className="logoItemsPhone">
+        <li><a href="https://www.facebook.com/mih.marian86" target="_blank" rel="noreferrer" className="logoItemPhone"> <FontAwesomeIcon icon={faFacebook} /> </a></li>
+        <p>&copy;Iași România 2020</p>
+        <li><a href="https://www.facebook.com/mih.marian86" target="_blank" rel="noreferrer" className="logoItemPhone"> <FontAwesomeIcon icon={faInstagram} /> </a></li>
+      </ul> 
+    </div>
+  )
+}
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import Nav_bar from "..//components/Nav_bar/Nav_bar"
 import Home from "..//components/Home/Home"
 import About from "..//components/About/About"
 import Projects from "..//components/Projects/Projects"
-import Form_contact from "../components/Form_contact/Form_contact"
+import Form_contact from "..//components/Form_contact/Form_contact"
+import Footer from "..//components/Footer/Footer"
 
 export default class index extends react.Component {
 
@@ -15,6 +16,7 @@ export default class index extends react.Component {
         <About />
         <Projects />
         <Form_contact />
+        <Footer />
       </div>
     )
   }
