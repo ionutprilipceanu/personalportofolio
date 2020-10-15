f0f8fe69e276ec02d4d7499b557f3a3a4dee0b49
ionutprilipceanu <ionutprilipceanu@users.noreply.github.com>
2020-10-15 19:19:34 +0300

    up

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,9 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { Card, Button} from 'react-bootstrap'
 import "./Projects.css"
 import Fade from "react-reveal/Fade"
-import photography_page from "..//About/images/photography_page.jpg"
-import handcrafted_leader from "..//About/images/handcrafted_leather.jpg"
-import filter_page from "..//About/images/filter_page.jpg"
+// import photography_page from "..//About/images/photography_page.jpg"
+// import handcrafted_leader from "..//About/images/handcrafted_leather.jpg"
+// import filter_page from "..//About/images/filter_page.jpg"
+import profilePhotoII from "..//About/images/profilePhotoII.jpg"
 
 
 
@@ -29,7 +30,7 @@ export class Projects extends Component {
               <div className="aboutContent">
 
               <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={photography_page} alt="photographyPage"/>
+                {/* <Card.Img variant="top" src={photography_page} alt="photographyPage"/> */}
                 <Card.Body>
                   <Card.Title>Photography</Card.Title>
                   <Card.Text>
@@ -43,7 +44,7 @@ export class Projects extends Component {
                 {/* <Card.Img variant="top" src={filter_page} alt="filterPage" /> */}
 
                 <div>
-                <img src={photography_page} alt="logo"></img>
+                <img src={profilePhotoII} alt="logo"></img>
                 </div>
 
                 <Card.Body>
@@ -56,7 +57,7 @@ export class Projects extends Component {
               </Card>
 
               <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={handcrafted_leader} alt="handcraftedLeader" />
+                {/* <Card.Img variant="top" src={handcrafted_leader} alt="handcraftedLeader" /> */}
                 <Card.Body>
                   <Card.Title>Handcrafted leather</Card.Title>
                   <Card.Text>
