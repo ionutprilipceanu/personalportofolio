d5a7cf8f0ff6e73f0a06a5d89065a980f7c3bbec
ionutprilipceanu <ionutprilipceanu@users.noreply.github.com>
2020-12-02 09:14:47 +0200

    up

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,6 +7,7 @@ import photographyPage from "..//Projects/images/profilePhotoA.jpg"
 import handcraftLeather from "..//Projects/images/profilePhotoB.jpg"
 import filterPage from "..//Projects/images/profilePhotoC.jpg"
 import passwordGenerator from "..//Projects/images/profilePhotoD.jpg"
+import psychologicalOffice from "..//Projects/images/profilePhotoE.jpg"
 
 
 
@@ -28,50 +29,65 @@ export class Projects extends Component {
             <Card.Body>
               <Card.Title className="abouttitle"><span className="border-bottom">Projects</span> </Card.Title>
               <div className="aboutContentProjects">
+                
+              <div className="firstContainer">
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src={photographyPage} alt="photographyPage"/>
+                  <Card.Body>
+                    <Card.Title>Photography</Card.Title>
+                    <Card.Text>
+                      HTML5, CSS3, JAVASCRIPT, jQuery
+                    </Card.Text>
+                    <Button variant="primary" className="send_Btn" href="https://ionut-prilipceanu.com/" target="_blank" >Let' check</Button>
+                  </Card.Body>
+                </Card>
 
-              <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={photographyPage} alt="photographyPage"/>
-                <Card.Body>
-                  <Card.Title>Photography</Card.Title>
-                  <Card.Text>
-                    HTML5, CSS3, JAVASCRIPT, jQuery
-                  </Card.Text>
-                  <Button variant="primary" className="send_Btn" href="https://ionut-prilipceanu.com/" target="_blank" >Let' check</Button>
-                </Card.Body>
-              </Card>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src={filterPage} alt="filterPage" />
+                  <Card.Body>
+                    <Card.Title>Filter App</Card.Title>
+                    <Card.Text>
+                      React JS, CSS3, Bootstrap
+                    </Card.Text>
+                    <Button variant="primary" className="send_Btn" href="https://photogallery-exercises.netlify.app/" target="_blank" >Let's check</Button>
+                  </Card.Body>
+                </Card>
 
-              <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={filterPage} alt="filterPage" />
-                <Card.Body>
-                  <Card.Title>Filter App</Card.Title>
-                  <Card.Text>
-                    React JS, CSS3, Bootstrap
-                  </Card.Text>
-                  <Button variant="primary" className="send_Btn" href="https://photogallery-exercises.netlify.app/" target="_blank" >Let's check</Button>
-                </Card.Body>
-              </Card>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src={handcraftLeather} alt="handcraftedLeader" />
+                  <Card.Body>
+                    <Card.Title>Handcrafted leather</Card.Title>
+                    <Card.Text>
+                      Gatsby, React JS, JAVASCRIPT, CSS3
+                    </Card.Text>
+                    <Button variant="primary" className="send_Btn" href="https://handcrafted-leather.netlify.app/" target="_blank" >Let's check</Button>
+                  </Card.Body>
+                </Card>
+              </div>
 
-              <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={handcraftLeather} alt="handcraftedLeader" />
-                <Card.Body>
-                  <Card.Title>Handcrafted leather</Card.Title>
-                  <Card.Text>
-                    Gatsby, React JS, JAVASCRIPT, CSS3
-                  </Card.Text>
-                  <Button variant="primary" className="send_Btn" href="https://handcrafted-leather.netlify.app/" target="_blank" >Let's check</Button>
-                </Card.Body>
-              </Card>
+              <div className="secondContainer">
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src={passwordGenerator} alt="passwordGenerator" />
+                  <Card.Body>
+                    <Card.Title>Password Generator</Card.Title>
+                    <Card.Text>
+                      HTML5, JAVASCRIPT, SCSS, Less, CSS3
+                    </Card.Text>
+                    <Button variant="primary" className="send_Btn" href="https://password-create.netlify.app/" target="_blank" >Let's check</Button>
+                  </Card.Body>
+                </Card>
 
-              <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={passwordGenerator} alt="passwordGenerator" />
-                <Card.Body>
-                  <Card.Title>Password Generator</Card.Title>
-                  <Card.Text>
-                    HTML5, JAVASCRIPT, SCSS, Less, CSS3
-                  </Card.Text>
-                  <Button variant="primary" className="send_Btn" href="https://password-create.netlify.app/" target="_blank" >Let's check</Button>
-                </Card.Body>
-              </Card>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src={psychologicalOffice} alt="psychologicalOffice" />
+                  <Card.Body>
+                    <Card.Title>Psychological Office</Card.Title>
+                    <Card.Text>
+                      Gatsby, React JS, JAVASCRIPT, CSS3
+                    </Card.Text>
+                    <Button variant="primary" className="send_Btn" href="https://www.psiholog-iasi-teleman.com/" target="_blank" >Let's check</Button>
+                  </Card.Body>
+                </Card>
+              </div>
                 
               </div>
 
