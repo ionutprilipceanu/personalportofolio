c26854fe9d1755e0427bbc6aee65541b270f778d
Ionut Prilipceanu <prilipceanu.ionut@gmail.com>
2023-03-31 15:48:29 +0300

    add new projects

--- src/components/Projects/items.js
+++ src/components/Projects/items.js
@@ -4,6 +4,9 @@ import filterPage from "..//Projects/images/profilePhotoC.jpg"
 import passwordGenerator from "..//Projects/images/profilePhotoD.jpg"
 import psychologicalOffice from "..//Projects/images/profilePhotoE.jpg"
 import onMovie from "..//Projects/images/profilePhotoF.jpg"
+import toDoList from "..//Projects/images/profilePhotoG.jpg"
+import snakeGame from "..//Projects/images/profilePhotoH.jpg"
+import slotGame from "..//Projects/images/profilePhotoI.jpg"
 
 export const items = [
   {
@@ -47,5 +50,26 @@ export const items = [
     title: "Filter App",
     Technologies: "React JS, CSS3, Bootstrap",
     link: "https://photogallery-exercises.netlify.app/",
+  },
+  {
+    key: 7,
+    src: snakeGame,
+    title: "Snake Game",
+    Technologies: "Angular TypeScript SCSS",
+    link: "https://world-snake-cup.netlify.app/",
+  },
+  {
+    key: 8,
+    src: slotGame,
+    title: "Slot Machine Game",
+    Technologies: "Angular TypeScript",
+    link: "",
+  },
+  {
+    key: 9,
+    src: toDoList,
+    title: "Task Tracker",
+    Technologies: "Angular TypeScript API",
+    link: "https://to-do-track.netlify.app/",
   },
 ]
